f0c7a63aca28e2396ed36bafb39546b6167fbdf9
Matthew Santeler <Matthew.Santeler@lendkey.com>
2019-08-15 15:40:22 -0400

    Burger

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {shallow} from 'enzyme/build';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-
-it('mounts without crashing', () => {
-  const wrapper = shallow(<App />);
-  wrapper.unmount()
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
--- frontend/src/views/ApiData/ApiData.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApiData from './ApiData';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ApiData />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
